294b7d3b69b21874bdbbd786c7686d5dd319f978
A B Shravan Krishna <abshravankrishna@gmail.com>
2023-09-04 16:28:23 +0530

    added simple expression evalutaion in calculator using regular expresssion library

---
+++ calculator.py
@@ -0,0 +1,28 @@
+import re
+
+def calculate(expression):
+    try:
+        # Use regular expressions to ensure valid input
+        expression = re.sub(r'[^0-9+\-*/() ]', '', expression)
+        result = eval(expression)
+        return result
+    except Exception as e:
+        return "Error: " + str(e)
+
+def main():
+    print("Python Calculator")
+    while True:
+        user_input = input("Enter an expression (or 'q' to quit): ")
+        
+        if user_input.lower() == 'q':
+            break
+        
+        if user_input.lower() == 'clear':
+            print("Cleared.")
+            continue
+        
+        result = calculate(user_input)
+        print("Result:", result)
+
+if __name__ == "__main__":
+    main()
